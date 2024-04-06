ad833d9fb5f88218200ddd21ad2701bff6208888
boydpelley <boydjrpelley@gmail.com>
2024-04-06 16:01:09 -0300

    start to fix program to properly generate statements

--- main.c
+++ main.c
@@ -2,7 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_LINE_LENGTH 1024
 
+void generateSQLInsert(const char *data[], int numColumns) {
+    printf("(");
+    for (int i = 0; i < numColumns; ++i) {
+        printf("'%s'", data[i]);
+        if (i < numColumns - 1)
+            printf(", ");
+    }
+    printf("),\n");
+}
 
 
 int main(int argc, char* argv[])
@@ -18,16 +28,36 @@ int main(int argc, char* argv[])
     char database_name[50];
     fgets(database_name, sizeof(database_name), stdin);
 
+    FILE *csvFile = fopen(argv[1], "r");
 
-
-
-
-    FILE *input = fopen(argv[1], "r");
-
-    if (input == NULL)
+    if (csvFile == NULL)
     {
         printf("Error with opening file.\n");
         return 1;
     }
+
+    char line[MAX_LINE_LENGTH];
+    int lineCount = 0;
+    printf("INSERT INTO defence (team_name, player_name, position, games_played, icetime_min, goals, "
+           "assists, first_assists, second_assists, points, shifts, expected_goals, shots_on_goal)\nVALUES\n");
+    while (fgets(line, sizeof(line), csvFile) && lineCount < 200) {
+        char *token;
+        const char *data[num_vals_per_row]; // Assuming maximum of 20 columns
+
+        // Parse CSV line
+        int numColumns = 0;
+        token = strtok(line, ",");
+        while (token != NULL && numColumns < num_vals_per_row) {
+            data[numColumns++] = token;
+            token = strtok(NULL, ",");
+        }
+
+        // Generate SQL INSERT statement
+        generateSQLInsert(data, numColumns);
+
+        lineCount++;
+    }
+    fclose(csvFile);
+    return 0;
 
 }
